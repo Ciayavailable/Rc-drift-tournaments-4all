f8a6e786fae13884396004118dde3b8d685835f1
Alex Dekt <a1.dk@yandex.ru>
2024-03-13 00:55:22 +0700

    Basic comands set.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats
 from aiogram.filters.command import Command
 from dotenv import dotenv_values
 
@@ -11,6 +12,24 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config["TG_TOKEN"])
 # Диспетчер
 dp = Dispatcher()
+
+
+async def set_bot_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Перезапустить бота"),
+        BotCommand(command="help", description="Помощь"),
+    ]
+    await bot.set_my_commands(commands=commands, scope=BotCommandScopeAllPrivateChats())
+
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Привет!")
+
+
+@dp.message(Command("help"))
+async def start(message: types.Message):
+    await message.answer("Ты молодец, у тебя все получится!")
 
 
 # Запуск процесса поллинга новых апдейтов
