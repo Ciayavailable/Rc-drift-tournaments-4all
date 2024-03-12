af28e0c61c92544a58c4c10b0498acbc7d52f3d4
Alex Dekt <a1.dk@yandex.ru>
2024-03-13 00:43:08 +0700

    Cleared bot.py from junk.

--- bot.py
+++ bot.py
@@ -12,31 +12,11 @@ bot = Bot(token=config["TG_TOKEN"])
 # Диспетчер
 dp = Dispatcher()
 
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    kb = [
-        [types.KeyboardButton(text="С пюрешкой")],
-        [types.KeyboardButton(text="Без пюрешки")]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
-
-# Хэндлер на команду /test1
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
-
-# Хэндлер на команду /test2
-@dp.message(Command("test2"))
-async def cmd_test2(message: types.Message):
-    await message.answer("Test 2")
-
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
